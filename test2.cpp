9821c9022599bf5f0996d3fd1fc6f1f15b32a6b1
zoe1adams <123134186+zoe1adams@users.noreply.github.com>
2025-03-27 15:46:45 -0400

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -1,1 +1,25 @@
+/*
+Without using classes, write a program to calculate the perimeter 
+of shapes. The program must accept from user input the number of sides
+the shape from the user and the length of each side, then calculate the 
+perimeter, and display the result. 
 
+Note: All sides are not necessarily same the length.
+Note: All sides are integers.
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+int main() {
+    cout<<"Part 1"<<endl; //do not remove, edit, or change the placement of this line.
+    
+//intergers
+int sideOne, sideTwo, sideThree, sideFour;
+// math equations
+perimeter = (sideOne + sideTwo +sideThree +sideFour);
+//
+cout >> perimeter;
+    return 0;
+ }
